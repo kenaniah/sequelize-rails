3e7832928244ecd4d37cd0f886ab60bbee119eb1
Kenaniah Cerny <kenaniah@gmail.com>
2021-12-26 20:54:06 -0800

    railties

--- lib/sequelize_rails/railtie.rb
+++ lib/sequelize_rails/railtie.rb
@@ -5,9 +5,12 @@ module SequelizeRails
   class Railtie < Rails::Railtie
     config.app_generators.orm :sequel, migration: :sequel_migration
 
+    initializer "sequel.plugins" do
+      ::Sequel::Model.plugin :active_model
+    end
+
     # https://api.rubyonrails.org/classes/ActiveModel/Translation.html
     initializer "sequel.i18n_support" do
-      ::Sequel::Model.plugin :active_model
       ::Sequel::Model.send :extend, ::ActiveModel::Translation
       ::Sequel::Model.send :extend, ::SequelizeRails::TranslationSupport
     end
