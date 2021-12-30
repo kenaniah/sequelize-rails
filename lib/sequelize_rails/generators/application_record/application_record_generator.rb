2aba6a2ed5ff5410dbfa1a648f81972244de3c43
Kenaniah Cerny <kenaniah@gmail.com>
2021-12-29 19:34:20 -0800

    creates migration files, but needs to implement attribute parsing

--- lib/sequelize_rails/generators/application_record/application_record_generator.rb
+++ lib/sequelize_rails/generators/application_record/application_record_generator.rb
@@ -2,11 +2,13 @@
 
 module SequelizeRails
   module Generators
-    class ApplicationRecordGenerator < ::Rails::Generators::NamedBase
+    class ApplicationRecordGenerator < ::Rails::Generators::Base
       source_root File.expand_path("templates", __dir__)
+
       def create_application_record
         template "application_record.rb", application_record_file_name
       end
+
       private
         def application_record_file_name
           @application_record_file_name ||=
--- lib/sequelize_rails/generators/migration/migration_generator.rb
+++ lib/sequelize_rails/generators/migration/migration_generator.rb
@@ -3,7 +3,18 @@
 module SequelizeRails
   module Generators
     class MigrationGenerator < ::Rails::Generators::NamedBase
+      include Rails::Generators::Migration
+
       source_root File.expand_path("templates", __dir__)
+
+      def self.next_migration_number _dirname
+        Time.now.strftime "%Y%m%d%H%M%S"
+      end
+
+      def create_migration_file
+        migration_template "migration.rb", "db/migrate/#{file_name}.rb"
+      end
+
     end
   end
 end
